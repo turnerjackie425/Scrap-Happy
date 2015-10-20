d3386ce7d50a8ec8c8ce5197d6fe24f29c4d8c5d
Jackie Turner <turnerjackie425@gmail.com>
2015-10-20 16:27:20 -0600

    devise errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :page_attachments
 
   root "page_attachments#index"
+  
 end
