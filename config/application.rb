5afcda9267ca8e26a25fb6459f1b25d1ba1fa84b
Jackie Turner <turnerjackie425@gmail.com>
2015-11-04 09:38:37 -0700

    sendgrid now completely working :). Need to finish image uploading/paperclip

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-ENV.update YAML.load_file('config/application.yml')[Rails.env] rescue {}
 
 module ScrapHappy
   class Application < Rails::Application
