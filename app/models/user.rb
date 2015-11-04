8b2dd03bd066e1bd1441069851ba3f0220eefb1a
Jackie Turner <turnerjackie425@gmail.com>
2015-11-04 12:21:02 -0700

    quick syntax fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :page_attachments
-  #has_attached_file :image, :styles => { :medium => "300x300>" }
- # validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
