3cdc1b2190407d990aaa376def684fb036e790ca
Jackie Turner <turnerjackie425@gmail.com>
2015-12-04 16:43:01 -0700

    picture sizing ccleanup

--- app/models/page_attachment.rb
+++ app/models/page_attachment.rb
@@ -1,6 +1,6 @@
 class PageAttachment < ActiveRecord::Base
   acts_as_votable
   belongs_to :user
-  has_attached_file :image, :styles => { :medium => "236x300>" }
+  has_attached_file :image, :styles => { :medium => "300x300>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
