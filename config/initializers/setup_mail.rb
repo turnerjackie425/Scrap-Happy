7d853dc78a35b630f2452c0aac3e29a028bb40ce
Jackie Turner <turnerjackie425@gmail.com>
2015-12-21 14:12:25 -0700

    removed lines from action mailer, attempting to fix sign in problem on heroku

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,5 +1,4 @@
-if Rails.env.development?
-  ActionMailer::Base.delivery_method = :smtp
+
   ActionMailer::Base.smtp_settings = {
     address:        'smtp.sendgrid.net',
     port:           '587',
@@ -9,4 +8,3 @@ if Rails.env.development?
     domain:         'heroku.com',
     enable_starttls_auto: true
   }
-end
