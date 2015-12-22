6826ab23d19a404a2dcc38eda4bea79352f8c4ef
Jackie Turner <turnerjackie425@gmail.com>
2015-12-22 15:00:56 -0700

    added sendgrid gem and changed env/prod.rb to fix heroku 404 problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {:host => 'smtp.sendgrid.net'}
+  config.action_mailer.default_url_options = {:host => 'heroku.com'}
   config.action_mailer.delivery_method = :smtp
 end
