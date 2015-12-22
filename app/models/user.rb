ccacc05941a177b43a96703d82b6effc3eb69cf3
Jackie Turner <turnerjackie425@gmail.com>
2015-12-22 14:57:49 -0700

    added sendgrid gem and changed env/prod.rb to fix heroku 404 problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :page_attachments
+
+  def welcome_message(user)
+    @user = user
+    mail(
+      :from => 'turnerjackie425@icloud.com',
+      :to => user.email,
+      :subject => 'Welcome to Scrap Happy!'
+      )
+  end
 end
